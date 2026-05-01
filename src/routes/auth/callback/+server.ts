f7434e8e12c33b21725538493c36dc51d86ca148
Hamadah <muhammadrozaan@gmail.com>
2026-05-01 20:33:48 +0700

    feat(bee): THERES BEE IN MY ROOM

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,17 +1,18 @@
-import { type RequestHandler } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ url, locals }) => {
-  const code = url.searchParams.get('code');
-  const next = url.searchParams.get('next') ?? '/';
+export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
+	const code = url.searchParams.get('code');
+	
+	if (code) {
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
+		
+		if (!error) {
+			throw redirect(303, '/');
+		} else {
+			console.error("❌ Gagal tukar kode OAuth:", error);
+		}
+	}
 
-  if (code) {
-    const { error } = await locals.supabase.auth.exchangeCodeForSession(code);
-    
-    if (!error) {
-      throw redirect(303, next);
-    }
-  }
-
-  throw redirect(303, '/login');
+	throw redirect(303, '/login');
 };
